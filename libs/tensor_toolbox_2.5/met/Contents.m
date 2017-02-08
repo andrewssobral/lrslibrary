3a3de30a6dac2bb39e23a60a6ff41607eee3c8db
Andrews Sobral <andrewssobral@gmail.com>
2017-02-08 19:39:35 +0100

    Added tensor_toolbox module

--- libs/tensor_toolbox_2.5/met/Contents.m
+++
@@ -1,8 +0,0 @@
-% MET: Memory Efficient Tucker
-%
-% Files
-%   ttm_me           - Memory-efficient sptensor times matrix.
-%   ttm_me_mem       - Estimates intermediate memory comsumption for ttm_me.
-%   ttm_me_partition - Finds best order for ttm_me.
-%   tucker_me        - Memory-efficient Tucker higher-order orthogonal iteration.
-%   tucker_me_test   - Very simple tests of tucker_me.
