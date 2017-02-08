0ce12e8d5fedada30b23f450e85271f646be3bfe
Andrews Sobral <andrewssobral@gmail.com>
2017-02-08 19:06:59 +0100

    Removing old vlfeat

--- libs/vlfeat-0.9.19/toolbox/xtest/vl_test_slic.m
+++
@@ -1,9 +0,0 @@
-function results = vl_test_slic(varargin)
-% VL_TEST_SLIC
-vl_test_init ;
-
-function s = setup()
-s.im = im2single(vl_impattern('roofs1')) ;
-
-function test_slic(s)
-segmentation = vl_slic(s.im, 10, 0.1) ;
